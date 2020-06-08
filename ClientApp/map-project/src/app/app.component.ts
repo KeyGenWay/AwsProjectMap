5e9dd2339cabaf7e4530c64417eb7bd402f48df2
Maciej <keygenmen@gmail.com>
2020-06-08 20:29:31 +0200

    Added map

--- ClientApp/map-project/src/app/app.component.ts
+++ ClientApp/map-project/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'map-project';
+
+  latitude = -28.68352;
+  longitude = -147.20785;
+  mapType = 'satellite';
 }
