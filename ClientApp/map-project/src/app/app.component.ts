3d55d0f930cb34dd4a37368df1a49400bef7028f
Maciej <keygenmen@gmail.com>
2020-06-14 23:21:12 +0200

    Implemented simple markers on Angular Maps

--- ClientApp/map-project/src/app/app.component.ts
+++ ClientApp/map-project/src/app/app.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
+import { MouseEvent } from '@agm/core'
+import { MarkerManager, AgmMarker } from '@agm/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements  AfterContentInit {
   title = 'map-project';
 
-  latitude = -28.68352;
-  longitude = -147.20785;
-  mapType = 'satellite';
+  latitude: number =51.9189046;
+  longitude: number =19.1343786;
+  mapType = 'hybrid';
+  public clickedPoint: AgmMarker = this.createMarker(this.longitude, this.latitude);
+
+  constructor(private httpClient: HttpClient) {
+  }
+  ngAfterContentInit(): void {
+    this.getLocation();
+  }
+
+  
+  public onRightClickHandle(event: MouseEvent ) {
+    const assign = 'anything';
+  
+  }
+
+  public onClickHandler(event: MouseEvent) {
+    this. clickedPoint = this.createMarker(event.coords.lng, event.coords.lat)
+  }
+  public getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=>{
+        this.setMapLocation(position.coords.longitude, position.coords.latitude);
+      });
+  } else {
+     console.log("No support for geolocation");
+    }
+  }
+
+  private setMapLocation(longitude: number, latitude: number)
+  {
+    this.clickedPoint = this.createMarker(longitude, latitude)
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  private createMarker(longitude: number, latitude: number){
+    return <AgmMarker> {
+      longitude: longitude,
+      latitude: latitude,
+      visible: true,
+      title: "This is my place"
+    }
+  }
 }
--- ClientApp/map-project/src/app/app.module.ts
+++ ClientApp/map-project/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AgmCoreModule } from '@agm/core';
+import { AgmCoreModule, MarkerManager, GoogleMapsAPIWrapper } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,11 @@ import { AgmCoreModule } from '@agm/core';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCWWwyU7qmp7WxzhvR07KhA0K4a7IEr0S0'
-    })
+    }),
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MarkerManager, GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
